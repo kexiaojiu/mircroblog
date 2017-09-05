9f199e560ee30e4962c03a1cb80e3d01a884d696
kexiaojiu <kexiaojiu@163.com>
2017-09-05 08:48:48 +0800

    添加登录页面

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 #coding=utf-8
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -21,3 +22,18 @@ def index():
     return render_template("index.html",
         title = 'Home',
         user = user)
+        
+        
+@app.route('/login', methods = ['GET', 'POST'])
+# index view function suppressed for brevity
+
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + 
+			'", remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html',
+        title = 'Sign In',
+        form = form,
+        providers = app.config['OPENID_PROVIDERS'])
