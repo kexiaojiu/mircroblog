0f8f161925f05c09e62c2dfeae8c4a5d3a947806
kexiaojiu <kexiaojiu@163.com>
2017-09-11 18:59:06 +0800

    #01 hello world

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,4 @@
-#!flask/bin/python
-
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_object('config')
-
 from app import views
--- run.py
+++ run.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python
-#coding=utf-8
+#!flask/bin/python
 
 from app import app
-
-app.run(debug=True)
+app.run(debug = True)
